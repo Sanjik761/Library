4c54fd4ab81be42f255616217afdc56a92c9606f
Sanjik761 <sanjarburiniyozov@gmail.com>
2021-08-25 11:27:34 +0500

    creat function addTableHeading

--- script.js
+++ script.js
@@ -12,3 +12,27 @@ class Book {
     this.status = !this.status;
   }
 }
+
+function addTableHeading() {
+  const body = document.querySelector('body');
+  if (document.querySelector('table')) {
+    body.removeChild(document.querySelector('table'));
+  }
+
+  const table = document.createElement('table');
+  const rowHead = document.createElement('tr');
+  const colHeadTitle = document.createElement('th');
+  colHeadTitle.textContent = 'Title';
+  rowHead.appendChild(colHeadTitle);
+  const colHeadAuthor = document.createElement('th');
+  colHeadAuthor.textContent = 'Author';
+  rowHead.appendChild(colHeadAuthor);
+  const colHeadPages = document.createElement('th');
+  colHeadPages.textContent = 'Pages';
+  rowHead.appendChild(colHeadPages);
+  const colHeadStatus = document.createElement('th');
+  colHeadStatus.textContent = 'Read Status';
+  rowHead.appendChild(colHeadStatus);
+
+  body.appendChild(table);
+}
