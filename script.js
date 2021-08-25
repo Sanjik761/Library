9fc4ecc2f3c8e3c934d607d9bc86328fed175c45
Sanjik761 <sanjarburiniyozov@gmail.com>
2021-08-25 11:26:16 +0500

    creat object js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 const myLibrary = [];
+
+class Book {
+  constructor(title, author, pages, status) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+  }
+
+  toggleRead() {
+    this.status = !this.status;
+  }
+}
